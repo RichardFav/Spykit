ebdba15f199caf299c857f47adc8db6c42993099
RichardFav <richard.faville@gmail.com>
2025-01-10 05:49:21 +0100

    Test commit

--- spike_pipeline/main.py
+++ spike_pipeline/main.py
@@ -1,34 +1,17 @@
-# This is a sample Python script.
+# module import
+import os
+import sys
+import time
+from PyQt5.QtWidgets import QApplication
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # sets the windows style (if on Subiculum)
+    if sys.platform == 'linux':
+        QApplication.setStyle('windows')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-# # module import
-# import os
-# import sys
-# import time
-# from PyQt5.QtWidgets import QApplication
-#
-# if __name__ == '__main__':
-#     # sets the windows style (if on Subiculum)
-#     if sys.platform == 'linux':
-#         QApplication.setStyle('windows')
-#
-#     # runs the analysis GUIs
-#     app = QApplication([])
-#     time.sleep(0.5)
-#     h_main = main_analysis.AnalysisGUI()
-#     time.sleep(0.5)
-#     app.exec()
+    # runs the analysis GUIs
+    app = QApplication([])
+    time.sleep(0.5)
+    h_main = main_analysis.AnalysisGUI()
+    time.sleep(0.5)
+    app.exec()
